7964ff08cfa1e697b9699aaaff6c6ebfd1535e0c
Matthew <mhornby123@gmail.com>
2023-06-29 20:07:37 +0100

    Added Upcoming movies page.

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,7 @@ import SiteHeader from "./components/siteHeader";
 import HomePage from "./pages/homePage";
 import MovieReviewPage from "./pages/movieReviewPage";
 import MoviePage from "./pages/movieDetailsPage";
+import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
     <BrowserRouter>
       <SiteHeader />
       <Routes>
+        <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
         <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/reviews/:id" element={<MovieReviewPage />} />
