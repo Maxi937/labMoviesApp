e60384c4396a7042c7c8dade2e700505170c3f1b
Matthew <mhornby123@gmail.com>
2023-06-30 22:30:02 +0100

    Using react-query to manage the browser-based cache.

--- src/components/templateMoviePage/index.jsx
+++ src/components/templateMoviePage/index.jsx
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import MovieHeader from "../headerMovie";
 import Grid from "@mui/material/Grid";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import { getMovieImages } from "../../api/tmdb-api";
+import { useQuery } from "react-query";
+import Spinner from "../spinner";
 
 const styles = {
   gridListRoot: {
@@ -13,19 +15,21 @@ const styles = {
   },
   gridList: {
     width: 450,
-    height: '100vh',
+    height: "100vh",
   },
 };
 
 const TemplateMoviePage = ({ movie, children }) => {
-  const [images, setImages] = useState([]);
+  const { data, error, isLoading, isError } = useQuery(["images", { id: movie.id }], getMovieImages);
 
-  useEffect(() => {
-    getMovieImages(movie.id).then((images) => {
-      setImages(images);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>;
+  }
+  const images = data.posters;
 
   return (
     <>
@@ -36,15 +40,8 @@ const TemplateMoviePage = ({ movie, children }) => {
           <div sx={styles.gridListRoot}>
             <ImageList cols={1}>
               {images.map((image) => (
-                <ImageListItem
-                  key={image.file_path}
-                  sx={styles.gridListTile}
-                  cols={1}
-                >
-                  <img
-                    src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
-                    alt={image.poster_path}
-                  />
+                <ImageListItem key={image.file_path} sx={styles.gridListTile} cols={1}>
+                  <img src={`https://image.tmdb.org/t/p/w500/${image.file_path}`} alt={image.poster_path} />
                 </ImageListItem>
               ))}
             </ImageList>
