4906cc8a29595da9af296a4625ec34ca6612f450
Matthew <mhornby123@gmail.com>
2023-08-04 18:52:15 +0100

    limit cast returned to 12

--- src/components/movieCredits/castList.jsx
+++ src/components/movieCredits/castList.jsx
@@ -4,10 +4,17 @@ import Grid from "@mui/material/Grid";
 import Actor from "./actor.jsx";
 
 const CastList = ({ castList }) => {
-  let actorCards = castList.map((actor) => 
-    <Grid item xs={1} sm={2} md={2} key={actor.id}>
-      <Actor actor={actor} />
-    </Grid>);
+  let actorCards = castList.map((actor) => {
+    if (actor.order >= 12) {
+      return;
+    }
+    return (
+      <Grid item xs={1} sm={2} md={2} key={actor.id}>
+        <Actor actor={actor} />
+      </Grid>
+    );
+  });
+
   return (
     <Grid container spacing={5}>
       {actorCards}
