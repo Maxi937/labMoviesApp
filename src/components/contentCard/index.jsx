a2cb072e698cb5abc0e3f86f34d231f755f3d01a
Matthew <mhornby123@gmail.com>
2023-08-07 17:50:29 +0100

    animated on images for pageswitching

--- src/components/contentCard/index.jsx
+++ src/components/contentCard/index.jsx
@@ -5,58 +5,64 @@ import Fade from "@mui/material/Fade";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import useImage from "../../hooks/useImage";
-import PlaceHolder from "../../images/film-poster-placeholder.png"
+import PlaceHolder from "../../images/film-poster-placeholder.png";
 
 const styles = {
   box: (backgroundImage) => {
-    const image = useImage(backgroundImage)
+    const image = useImage(backgroundImage);
 
-    return image ? {
-      borderRadius: 5,
-      boxShadow: 5,
-      overflow: "hidden",
-      zIndex: 1,
-      display: "flex",
-      position: "relative",
-      width: 200,
-      height: 300,
-      backgroundRepeat: "no-repeat",
-      backgroundSize: "cover",
-      "&::before": {
-        content: `""`,
-        backgroundImage: `url(${image})`,
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center center",
-        backgroundSize: "cover",
-        position: "absolute",
-        top: 0,
-        right: 0,
-        bottom: 0,
-        left: 0,
-        transition: "filter 0.3s ease 0s",
-      },
-      "&:hover:before": {
-        filter: "brightness(75%)",
-      },
-    } : {
-      borderRadius: 5,
-      boxShadow: 5,
-      overflow: "hidden",
-      zIndex: 1,
-      display: "flex",
-      position: "relative",
-      width: 200,
-      height: 300,
-      backgroundImage: `url(${PlaceHolder})`,
-      backgroundSize: "cover",
-      backgroundPosition: "center center",
-      backgroundColor: "grey",
-      animation: "blur 2s",
-      "@keyframes blur": {
-        "100%": { filter: "blur(8px)" },
-        "0%": { filter: "blur(0)" }
-      },
-    }
+    return image
+      ? {
+          borderRadius: 5,
+          boxShadow: 5,
+          overflow: "hidden",
+          zIndex: 1,
+          display: "flex",
+          position: "relative",
+          width: 200,
+          height: 300,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          "&::before": {
+            animation: "blur 1s",
+            "@keyframes blur": {
+              "0%": { filter: "blur(8px)" },
+              "100%": { filter: "blur(0px)" },
+            },
+            content: `""`,
+            backgroundImage: `url(${image})`,
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center center",
+            backgroundSize: "cover",
+            position: "absolute",
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+            transition: "filter 0.3s ease 0s",
+          },
+          "&:hover:before": {
+            filter: "brightness(75%)",
+          },
+        }
+      : {
+          borderRadius: 5,
+          boxShadow: 5,
+          overflow: "hidden",
+          zIndex: 1,
+          display: "flex",
+          position: "relative",
+          width: 200,
+          height: 300,
+          backgroundColor: "white",
+          backgroundSize: "cover",
+          backgroundPosition: "center center",
+          animation: "blur 3s",
+          "@keyframes blur": {
+            "100%": { filter: "blur(8px)" },
+            "0%": { filter: "blur(3px)" },
+          },
+        };
   },
   overlay: {
     display: "flex",
@@ -66,12 +72,11 @@ const styles = {
     "&:hover": {
       cursor: "pointer",
     },
-
   },
 };
 
 export default function ContentCard({ content, action }) {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [active, setActive] = useState(false);
 
   function handleMouseOver() {
@@ -84,8 +89,8 @@ export default function ContentCard({ content, action }) {
 
   function handleClick(e) {
     e.preventDefault();
-    e.stopPropagation()
-    content.first_air_date ?  navigate(`/tv/${content.id}`) : navigate(`/movies/${content.id}`)
+    e.stopPropagation();
+    content.first_air_date ? navigate(`/tv/${content.id}`) : navigate(`/movies/${content.id}`);
   }
 
   return (
