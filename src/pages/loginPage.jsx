9c286d2e023715bae69f88495cf744e051d74e88
Matthew <mhornby123@gmail.com>
2023-08-11 17:50:57 +0100

    login page

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -3,7 +3,16 @@ import Login from "../components/loginForm/index";
 import SignupForm from "../components/loginForm/signupForm";
 import { Grid } from "@mui/material";
 
+const styles = {
+  root: {
+    display: "flex"
+  }
+}
 const LoginPage = (props) => {
-  return <Login />;
+  return (
+    <Box sx={styles.root}>
+      <Login />
+    </Box>
+  );
 };
 export default LoginPage;
