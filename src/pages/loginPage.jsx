12ae33f85e1c384f18cf181db5ecccd3c1ac91b4
Matthew <mhornby123@gmail.com>
2023-08-11 17:43:31 +0100

    Login page

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,15 +1,9 @@
 import React, { useState, useEffect } from "react";
-import Login from "../components/loginForm/index"
+import Login from "../components/loginForm/index";
 import SignupForm from "../components/loginForm/signupForm";
 import { Grid } from "@mui/material";
 
 const LoginPage = (props) => {
-  return (
-    <Grid container spacing={2}>
-      <Grid item xs>
-          <Login />
-      </Grid>
-    </Grid>
-  );
+  return <Login />;
 };
 export default LoginPage;
