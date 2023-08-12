2d88ae8ef643c88b4abf29c1c301dab6cf63224c
Matthew <mhornby123@gmail.com>
2023-08-12 12:07:33 +0100

    limit tv photos on details page

--- src/components/templateTvPage/index.jsx
+++ src/components/templateTvPage/index.jsx
@@ -38,11 +38,16 @@ const TemplateTvPage = ({ tvShow, children }) => {
         <Grid item xs={3}>
           <div sx={styles.gridListRoot}>
             <ImageList cols={1}>
-              {images.map((image) => (
-                <ImageListItem key={image.file_path} sx={styles.gridListTile} cols={1}>
-                  <img src={`https://image.tmdb.org/t/p/w500/${image.file_path}`} alt={image.poster_path} />
-                </ImageListItem>
-              ))}
+              {images.map((image, index) => {
+                if (index > 8) {
+                  return;
+                }
+                return (
+                  <ImageListItem key={image.file_path} sx={styles.gridListTile} cols={1}>
+                    <img src={`https://image.tmdb.org/t/p/w500/${image.file_path}`} alt={image.poster_path} />
+                  </ImageListItem>
+                );
+              })}
             </ImageList>
           </div>
         </Grid>
